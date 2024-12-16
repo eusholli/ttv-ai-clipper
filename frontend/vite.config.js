deea101ed3bec7db052d98485334d30586d69232
Geoff Hollingworth <eusholli@gmail.com>
2024-12-16 09:46:32 +0000

    debug vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
+  
+  // Load and log all env vars during build
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('Build Mode:', mode)
+  console.log('Environment Variables:', env)
   
   return {
     plugins: [react()],
